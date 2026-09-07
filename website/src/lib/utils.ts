4303f2f4212ee2f92a227e0a0c8089fae49bfe56
Ersin KOÇ <ersinkoc@gmail.com>
2026-09-08 01:46:17 +0300

    chore(release): align build dependencies and package metadata

--- website/src/lib/utils.ts
+++ website/src/lib/utils.ts
@@ -12,7 +12,7 @@ export function cn(...inputs: ClassValue[]) {
    ========================================================================= */
 
 export const META = {
-  version: '1.0.2',
+  version: '1.0.3',
   repo: 'https://github.com/WrongStack/WrongStack',
   npm: 'wrongstack',
   node: '22',
